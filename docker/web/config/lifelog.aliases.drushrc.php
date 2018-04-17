9d049a68f09991a070c51931dc6315a4969f1f40
Brendan Anderson <bander2.imda@gmail.com>
2018-04-17 15:34:01 -0400

    Initial commit

---
+++ docker/web/config/lifelog.aliases.drushrc.php
@@ -0,0 +1,9 @@
+<?php
+
+$aliases['lifelog'] = array(
+  'root' => '/var/www/drupal/web',
+  'uri'  => 'lifelog.andersonproject.com',
+  'path-aliases' => array(
+    '%dump-dir' => '/tmp',
+  ),
+);
