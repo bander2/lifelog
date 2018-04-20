e870a17e5417c35e0cbaf8c700080baa02f93f1e
Brendan Anderson <bander2.imda@gmail.com>
2018-04-19 20:14:06 -0400

    Automate update process

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -2,4 +2,8 @@
 
 chown -R www-data:www-data /var/www/drupal/sites/default/files
 
+drush cim -n -q -y --root=/var/www/drupal/web
+drush updb -n -q -y --root=/var/www/drupal/web
+drush cr -n -q -y --root=/var/www/drupal/web
+
 exec "$@"
